7628118675e5bcafd212f7f1bb3006857b62d7f4
rehoosa <iuhoosa@gmail.com>
2021-06-07 21:12:03 +0900

    Add MyRecipe Android Project
    프로젝트 생성함
    xml파일들만 만들어놓음

---
+++ rehoosa/MyRecipe/app/src/main/java/com/example/myrecipe/activity/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myrecipe.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myrecipe.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
