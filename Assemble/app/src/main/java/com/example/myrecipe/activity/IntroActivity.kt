4d1a85291174972c464114daf412a62279879f9c
rehoosa <iuhoosa@gmail.com>
2021-06-09 18:59:13 +0900

    Update IntroActivity.kt

--- Assemble/app/src/main/java/com/example/myrecipe/activity/IntroActivity.kt
+++ Assemble/app/src/main/java/com/example/myrecipe/activity/IntroActivity.kt
@@ -1,16 +1,39 @@
 package com.example.myrecipe.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myrecipe.R
+import com.example.myrecipe.databinding.ActivityIntroBinding
 
 // 로딩화면
 // 개요 : 로딩화면으로 로고를 보여줌
 // 기능 : db정보를 받아오는 등의 작업을 진행한다
 
 class IntroActivity : AppCompatActivity() {
+    lateinit var binding:ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityIntroBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        setContentView(binding.root)
+        init()
+    }
+
+    fun init(){
+        with(binding){
+            BtnAddRecipeActivity.setOnClickListener {
+                val intent = Intent(this@IntroActivity, AddRecipeActivity::class.java)
+                startActivity(intent)
+            }
+            BtnSearchActivity.setOnClickListener {
+                val intent = Intent(this@IntroActivity, SearchRecipeActivity::class.java)
+                startActivity(intent)
+            }
+            BtnSelectActivity.setOnClickListener {
+                val intent = Intent(this@IntroActivity, SelectIngredientsActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
